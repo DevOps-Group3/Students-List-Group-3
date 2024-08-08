65b26cabbc5006cd7ff52ad327257c9b173ee3d1
Piyumika Samarasuriyage <piyumikabandula@gmail.com>
2024-08-08 13:49:43 -0400

    Initial commit - Simple React app with student names and IDs

---
+++ src/StudentList.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const students = [
+  { id: 'C0899391', name: 'Yubraj Rai' },
+  { id: 'C0901142', name: 'Roshan Shrestha' },
+  { id: 'C0901297', name: 'Fuchun Li' },
+  { id: 'C0901339', name: 'Pauline Balamores' },
+  { id: 'C0900440', name: 'Piyumika Samarasuriyage' }
+];
+
+const StudentList = () => {
+  return (
+    <div>
+      <h1>Group 3 - Student List</h1>
+      <ul>
+        {students.map((student) => (
+          <li key={student.id}>
+            {student.name} - {student.id}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default StudentList;
