252530406eaae7ad7db1eabdc05f45adc1126424
Piyumika Samarasuriyage <piyumikabandula@gmail.com>
2024-08-13 08:23:50 -0400

    Modify test cases to suit the app

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,24 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders the correct header', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headerElement = screen.getByText(/DevOps Tools and Practices 03: Group 3/i);
+  expect(headerElement).toBeInTheDocument();
+});
+
+test('renders student names and IDs', () => {
+  render(<App />);
+  const student1 = screen.getByText(/Yubraj Rai - C0899391/i);
+  const student2 = screen.getByText(/Roshan Shrestha - C0901142/i);
+  const student3 = screen.getByText(/Fuchun Li - C0901297/i);
+  const student4 = screen.getByText(/Pauline Balamores - C0901339/i);
+  const student5 = screen.getByText(/Piyumika Samarasuriyage - C0900440/i);
+
+  expect(student1).toBeInTheDocument();
+  expect(student2).toBeInTheDocument();
+  expect(student3).toBeInTheDocument();
+  expect(student4).toBeInTheDocument();
+  expect(student5).toBeInTheDocument();
 });
